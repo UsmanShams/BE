3a80f4a24406fd87f19bdd2cffbf88045b91d90f
UsmanShams <usmanshams31@gmail.com>
2026-08-12 22:25:42 +0500

    test

--- obj/Debug/net6.0/be.AssemblyInfo.cs
+++ obj/Debug/net6.0/be.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26987d1eea58dba14353f451ce14b36493c0422")]
 [assembly: System.Reflection.AssemblyProductAttribute("be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
